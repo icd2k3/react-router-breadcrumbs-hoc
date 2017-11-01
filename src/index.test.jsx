9b8eb22b1485a87a591f73e72d4e43422abda7c5
JUSTIN SCHRADER <icd2k3@gmail.com>
2017-10-31 17:18:28 -0700

    Add test

--- src/index.test.jsx
+++ src/index.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { mount } from 'enzyme';
 import { StaticRouter as Router } from 'react-router';
+import { NavLink } from 'react-router-dom';
 import { withBreadcrumbs } from './index';
 
 const components = {
@@ -11,6 +12,7 @@ const components = {
     </div>
   ),
   BreadcrumbMatchTest: ({ match }) => <span>{match.params.number}</span>,
+  BreadcrumbNavLinkTest: ({ match }) => <NavLink to={match.url}>Link</NavLink>,
 };
 
 const matchShape = {
@@ -31,22 +33,30 @@ components.BreadcrumbMatchTest.propTypes = {
   match: PropTypes.shape(matchShape).isRequired,
 };
 
+components.BreadcrumbNavLinkTest.propTypes = {
+  match: PropTypes.shape(matchShape).isRequired,
+};
+
+// randomize routes in array to make sure order doesn't matter
+const shuffle = arr => arr.sort(() => Math.random() - 0.5);
+
 describe('react-router-breadcrumbs-hoc', () => {
   describe('withBreadcrumbs', () => {
-    const routes = [
-      { path: '/no-match', breadcrumb: 'no match' },
+    const routes = shuffle([
       // test breadcrumb passed as string
       { path: '/1', breadcrumb: '1' },
       // test simple breadcrumb component
       { path: '/1/2', breadcrumb: () => <span>TWO</span> },
       // test advanced breadcrumb component (user can use `match` however they wish)
       { path: '/1/2/:number', breadcrumb: components.BreadcrumbMatchTest },
+      // test NavLink wrapped breadcrumb
+      { path: '/1/2/:number/4', breadcrumb: components.BreadcrumbNavLinkTest },
       // test a no-match route
       { path: '/no-match', breadcrumb: 'no match' },
-    ];
+    ]);
     const routerProps = {
       context: {},
-      location: { pathname: '/1/2/3' },
+      location: { pathname: '/1/2/3/4' },
     };
 
     it('Should render breadcrumb components as expected', () => {
@@ -54,6 +64,7 @@ describe('react-router-breadcrumbs-hoc', () => {
       const wrapper = mount(<Router {...routerProps}><ComposedComponent /></Router>);
 
       expect(wrapper.find(ComposedComponent)).toMatchSnapshot();
+      expect(wrapper.find(NavLink).props().to).toBe('/1/2/3/4');
     });
   });
 });
