c2b6d1a9d9429a4b62519650492e1b47f82f66f5
Justin Schrader <icd2k3@gmail.com>
2018-09-19 21:55:08 -0700

    Upgrades devDependencies

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+module.exports = function(api) {
+  api.cache(true);
+
+  return {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react',
+    ],
+    plugins: [],
+  };
+};
