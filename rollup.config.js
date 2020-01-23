7c40570ae12b83cf48f7ea8c9d604d8b469f7be8
Justin Schrader <jschrader@newrelic.com>
2020-01-23 13:12:23 -0800

    chore: update dev dependencies + set nvm version to latest LTS version

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import { uglify } from 'rollup-plugin-uglify';
 
 const pkg = require('./package.json');
